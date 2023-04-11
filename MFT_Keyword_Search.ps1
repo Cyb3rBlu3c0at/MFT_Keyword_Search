3582d05e8a4b35ff95abc39e04e1a96ed51c8973
Cyb3rBlu3c0at <68782083+Cyb3rBlu3c0at@users.noreply.github.com>
2023-04-11 06:18:06 -0700

    Add files via upload

---
+++ MFT_Keyword_Search.ps1
@@ -0,0 +1,63 @@
+﻿<#
+Description: PowerShell script to automate the search of the MFT csv file created after the use of MFTECmd.
+
+Author: Mike Dunn
+
+Creation Date: 4/3/2023
+
+Version: 1
+
+NOTE: Requires a keyword text file.
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+$fileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$fileDialog.Title = "Select Keyword Text File"
+$fileDialog.Filter = "TXT files (*.txt)|*.txt"
+
+if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $KeywordFilePath = $fileDialog.FileName
+    Write-Host "Selected file: $KeywordFilePath"
+} else {
+    Write-Host "File selection canceled"
+    break
+}
+
+Add-Type -AssemblyName System.Windows.Forms
+$fileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$fileDialog.Title = "Select MFT CSV file"
+$fileDialog.Filter = "CSV files (*.csv)|*.csv"
+
+if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $MFTFilePath = $fileDialog.FileName
+    Write-Host "Selected file: $MFTFilePath"
+} else {
+    Write-Host "File selection canceled"
+    break
+}
+
+$keywordList = New-Object System.Collections.ArrayList
+Get-Content $KeywordFilePath | ForEach-Object { $keywordList.Add($_) | Out-Null }
+
+Write-Host "Importing MFT CSV File, please wait..."
+$csv = Import-Csv $MFTFilePath
+
+$totalRows = $csv.Count
+$currentRow = 0
+$percentComplete = 0
+Write-Progress -Activity "Searching for matches" -Status "Processing row $currentRow of $totalRows" -PercentComplete $percentComplete
+
+
+foreach ($row in $csv) {
+    foreach ($keyword in $keywordList) {
+        if ($row.FileName -ieq $keyword) {
+            $row | Export-Csv -Path "$env:UserProfile\Desktop\Results.csv" -Append -Force -NoTypeInformation
+            break
+        }
+    }
+    $currentRow++
+    $percentComplete = [int]($currentRow / $totalRows * 100)
+    Write-Progress -Activity "Searching for matches" -Status "Processing row $currentRow of $totalRows" -PercentComplete $percentComplete
+}
+
+Write-Progress -Activity "Searching for matches" -Status "Complete" -PercentComplete 100
